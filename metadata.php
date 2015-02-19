635b3f8b1657d675056a818ed50b0f260be8808a
Robin Lehrmann <role@vkf-renzel.de>
2015-02-19 17:10:32 +0100

    adding dynamic theme settings

--- metadata.php
+++ metadata.php
@@ -12,8 +12,8 @@ $aModule = array(
     'id'          => 'raless',
     'title'       => 'ra less',
     'description' => array(
-        'de'    => 'Kompiliert LESS-Dateien',
-        'en'    => 'Compiles LESS files',
+        'de'    => 'Kompiliert LESS-Dateien in Themes und Modulen',
+        'en'    => 'Compiles LESS files in themes and modules',
     ),
     'email'         => 'math@vkf-renzel.de',
     'url'           => 'http://www.renzel-agentur.de/',
@@ -28,9 +28,22 @@ $aModule = array(
             'template'                  => 'layout/base.tpl',
             'block'                     => 'base_style',
             'file'                      => 'views/ra/blocks/tpl/layout/base.tpl'
+        ),
+        array(
+            'template'                  => 'theme_config.tpl',
+            'block'                     => 'admin_theme_config_form',
+            'file'                      => 'views/admin/blocks/tpl/theme_config.tpl'
         )
     ),
     'files' => array(),
+    'settings' => array(
+        array(
+            'group' => 'main',
+            'name'  => 'sVariables',
+            'type'  => 'str',
+            'value' => ''
+        )
+    ),
     'events' => array(
         'onActivate'    => 'ralessevents::onActivate',
         'onDeactivate'  => 'ralessevents::onDeactivate'
---
+++ views/admin/de/less_lang.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * less_lang.php
+ *
+ * @version   GIT: $Id$ PHP5.4 (03.02.2015)
+ * @author    Robin Lehrmann <info@renzel-agentur.de>
+ * @copyright Copyright (C) 22.10.2014 renzel.agentur GmbH. All rights reserved.
+ * @license   http://www.renzel-agentur.de/licenses/raoxid-1.0.txt
+ * @link      http://www.renzel-agentur.de/
+ *
+ */
+
+$sLangName = "Deutsch";
+
+$aLang     = array(
+    'charset'                => 'utf-8',
+    'SHOP_MODULE_sVariables' => 'Geben Sie hier (Kommata getrennt) Ihre verwendeten less Variablen an, damit die Einstellungen aus dem Aktuellen Theme übernommen werden können.'
+);
